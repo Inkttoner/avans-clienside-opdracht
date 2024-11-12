e2cfced8b8082a8f47d3d7d846286f51f872c0ed
Coen <kruijf.coen@gmail.com>
2024-11-12 13:41:46 +0100

    initial commit

---
+++ apps/my-app/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { appRoutes } from './app.routes';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/ui/footer/footer.component';
+import { HeaderComponent } from './components/ui/header/header.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        DashboardComponent,
+        AboutComponent,
+        FooterComponent,
+        HeaderComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(appRoutes, {
+            initialNavigation: 'enabledBlocking'
+        })
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule {}
