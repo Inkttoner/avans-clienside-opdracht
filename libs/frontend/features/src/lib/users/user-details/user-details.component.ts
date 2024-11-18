e54ef626b0708228d71da0e362f60f5911ce91bb
Coen <kruijf.coen@gmail.com>
2024-11-18 13:15:45 +0100

    Added dashboard page and updated about

--- libs/frontend/features/src/lib/users/user-details/user-details.component.ts
+++ libs/frontend/features/src/lib/users/user-details/user-details.component.ts
@@ -1,30 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { IUserInfo } from '@avans-nx-workshop/shared/api';
 import { ActivatedRoute } from '@angular/router';
-import { UserService } from'../user.sevice';
-
+import { UserService } from '../user.sevice';
 
 @Component({
     selector: 'avans-nx-workshop-user-details',
     templateUrl: './user-details.component.html',
     styles: []
 })
-export class UserDetailsComponent implements OnInit {
-    userId: string | null = null;
+export class UserDetailsComponent implements OnInit, OnDestroy {
     user: IUserInfo | null = null;
 
     constructor(
         private userService: UserService,
         private route: ActivatedRoute
-    ) { }
+    ) {}
 
     ngOnInit(): void {
         this.route.paramMap.subscribe((params) => {
-        this.userId = params.get('id');
-        this.user = this.userService.getUserById(String(this.userId));    
-    });
-
-    };
-
-
+            var userId = params.get('id');
+            this.user = this.userService.getUserById(String(userId));
+        });
+    }
+    ngOnDestroy(): void {
+        
+    }
 }
